0d265776ea9eee83e5a113041354aefa265b957d
ShubhamSingh3102 <shubham18singh07@gmail.com>
2026-05-30 00:26:35 +0530

    backend completed

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -109,15 +109,21 @@ export const stripeWebhooks = async (request, response) => {
 
                 const { purchaseId } = session.data[0].metadata;
 
+                if (!purchaseData || purchaseData.status === "completed") break;
+
                 const purchaseData = await Purchase.findById(purchaseId)
                 const userData = await User.findById(purchaseData.userId)
                 const courseData = await Course.findById(purchaseData.courseId.toString());
 
-                courseData.enrolledStudents.push(userData._id)
-                await courseData.save()
+                if (!courseData.enrolledStudents.includes(userData._id)) {
+                    courseData.enrolledStudents.push(userData._id);
+                    await courseData.save();
+                }
 
-                userData.enrolledCourses.push(courseData._id)
-                await userData.save()
+                if (!userData.enrolledCourses.includes(courseData._id)) {
+                    userData.enrolledCourses.push(courseData._id);
+                    await userData.save();
+                }
 
                 purchaseData.status = 'completed'
                 await purchaseData.save()
